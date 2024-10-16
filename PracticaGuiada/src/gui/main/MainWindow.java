b0cbd6f6a653d6067a8011671296a8b0bec961dd
Asier Amigorena <asier.amigorena@opendeusto.es>
2024-10-16 10:26:47 +0200

    Ejercicio 1

---
+++ PracticaGuiada/src/gui/main/MainWindow.java
@@ -0,0 +1,20 @@
+package gui.main;
+
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame{
+	
+	public MainWindow() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("JJ.OO. París 2024");
+		setSize(640, 480);
+		setLocationRelativeTo(null);
+		
+		
+		
+		setVisible(true);
+	}
+
+}
---
+++ PracticaGuiada/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import gui.main.MainWindow;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MainWindow window = new MainWindow();
+	}
+
+}
